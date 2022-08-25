2263f10379f5ab904a163ad561b7c06f9ea2cf5e
shrishail <shrikariagr92@gmail.com>
2022-08-25 21:24:18 +0530

    added:addressbook functionality

---
+++ src/com/addressbook/MainAddressbook.java
@@ -0,0 +1,19 @@
+package com.addressbook;
+
+import java.util.ArrayList;
+
+public class MainAddressbook {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("Wel-Come To AddressBook Project");
+		
+	
+		 AddressBook addressbook =new AddressBook();
+		 addressbook.operation();
+		 
+		 
+	}
+	
+	
+}
